4c02c291c09c63f74225d69e06c5c7701dac78e1
davidr <davidrowthorn@gmail.com>
2019-02-18 16:23:30 +0000

    colStrings uses recursion

--- src/modules/sequence/SequenceCombiner/SequenceCombiner.js
+++ src/modules/sequence/SequenceCombiner/SequenceCombiner.js
@@ -1,65 +1,75 @@
-export default class SeqeuenceCombiner {
-    combine = sequences => {
-        let result = ''
-        const s = sequences.slice()
-        const columns = this._toColumns(s)
+// export const combineSeqeunces = sequences => {
+//     let result = ''
+//     const s = sequences.slice()
+//     const columns = toColumns(s)
 
-        while (columns.length) {
-            result += this._combineNotes(
-                columns.shift()
-            )
+//     while (columns.length) {
+//         result += combineNotes(
+//             columns.shift()
+//         )
+//     }
+//     return result
+// }
+
+// const combineNotes = column => {
+//     column = column.split('').sort().join('')
+
+//     const invalids = column.match(/[^a-gz]/gi)
+//     if (invalids) {
+//         const list = invalids
+//             .map(i => "'" + i + "'")
+//             .join(', ')
+//         throw new Error('Unsupported characters: ' + list)
+//     }
+
+//     if (column.search(/[a-g]+/gi) === -1) {
+//         return column.slice(-1)
+//     }
+
+//     column = removeRests(column)
+//     column = removeDuplicates(column)
+//     column = addBrackets(column)
+
+//     return column
+// }
+
+export const toColumns = arr => {
+    const result = [];
+    const desc = arr.sort((a, b) => b.length - a.length);
+    let i = desc.length;
+    while (i--) {
+        for (let j = 0; j < desc[i].length; j++) {
+            let note = desc[i][j];
+            result[j] = result[j]
+                ? note + result[j]
+                : note;
         }
-        return result
     }
-
-    _combineNotes = column => {
-        column = column.split('').sort().join('')
-
-        const invalids = column.match(/[^a-gz]/gi)
-        if (invalids) {
-            const list = invalids
-                .map(i => "'" + i + "'")
-                .join(', ')
-            throw new Error('Unsupported characters: ' + list)
-        }
-
-        if (column.search(/[a-g]+/gi) === -1) {
-            return column.slice(-1)
-        }
-
-        column = this._removeRests(column)
-        column = this._removeDuplicates(column)
-        column = this._addBrackets(column)
-
-        return column
-    }
-
-    _toColumns = arr => {
-        const result = [];
-        const desc = arr.sort((a, b) => b.length - a.length);
-        let i = desc.length;
-        while (i--) {
-            for (let j = 0; j < desc[i].length; j++) {
-                let note = desc[i][j];
-                result[j] = result[j]
-                    ? note + result[j]
-                    : note;
-            }
-        }
-        return result
-    }
-
-    _removeDuplicates = column => {
-        let result = ''
-        for (let i = 0; i < column.length; i++) {
-            if (!result.includes(column[i])) {
-                result += column[i]
-            }
-        }
-        return result
-    }
-
-    _addBrackets = column => column.replace(/[a-g]{2,}/gi, '[$&]') 
-
-    _removeRests = column => column.replace(/z/gi, '')
+    return result
 }
+
+const toString = column => [column.reduce((total, note) => total + note, '')] 
+
+const sortedToColStrings = sequences => {
+    return sequences.length
+        ? sequences[0].length <= 1
+            ? toString(sequences)
+            : toString(sequences.map(s => s.substr(0, 1)))
+                .concat(sortedToColStrings(sequences.map(s => s.substr(1))))
+        : []
+}
+
+export const toColStrings = sequence =>
+    sortedToColStrings(
+        sequence.sort(
+            (a, b) => b.length - a.length
+        )
+    )
+
+
+
+export const removeDuplicates = column => [...new Set(column.split(''))].join('')
+
+const addBrackets = column => column.replace(/[a-g]{2,}/gi, '[$&]')
+
+const removeRests = column => column.replace(/z/gi, '')
