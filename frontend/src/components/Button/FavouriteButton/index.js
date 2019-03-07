b04436e2b15f2ff163ed5feb53c6a67e5704140e
davidr <davidrowthorn@gmail.com>
2019-03-07 21:09:34 +0000

    async await not really working for me

--- frontend/src/components/Button/FavouriteButton/index.js
+++ frontend/src/components/Button/FavouriteButton/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const FavouriteButton = () => {
+
   return (
     <>
-      <button>Add to favourites</button>
+      <button onClick={() => { getShit }}>Add to favourites</button>
     </>
   )
+}
+
+const getShit = async () => {
+  const url = process.env.REACT_APP_API_GATEWAY_URL
+  await fetch(url)
 }
 
 export default FavouriteButton
