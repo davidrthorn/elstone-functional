846b45e95a29b512e15a88d123a80d00b6c7dc9d
davidr <davidrowthorn@gmail.com>
2019-03-08 18:04:50 +0000

    restructure

--- src/components/Button/FavouriteButton/index.js
+++ src/components/Button/FavouriteButton/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 const FavouriteButton = () => {
-
   return (
     <>
-      <button onClick={() => { getShit }}>Add to favourites</button>
+      <button onClick={getShit}>Add to favourites</button>
     </>
   )
 }
 
 const getShit = async () => {
   const url = process.env.REACT_APP_API_GATEWAY_URL
-  await fetch(url)
+  const res = await window.fetch(url)
+  console.log(res)
 }
 
 export default FavouriteButton
